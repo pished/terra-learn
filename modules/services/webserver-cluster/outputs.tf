c74f80fa8af1fee8520a1295c3f0bd99a8723632
pished <mimyers@roguecontract.com>
2021-05-16 19:02:00 -0400

    chapter 5 tidbits

--- modules/services/webserver-cluster/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "domain name of load balancer"
-}
-
-output "asg_name" {
-  value = aws_autoscaling_group.sample.name
-  description = "The name of the auto scaling group"
-}
